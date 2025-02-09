1507c62bd0fd6ec005aeca43e9722dfac9df8d61
samTime101 <srijanregmi44@gmail.com>
2025-02-09 18:26:35 +0545

    feat: arrow functionality

--- main.py
+++ main.py
@@ -5,32 +5,54 @@ readline for navigating cursor in a line
 import readline
 # import keyboard
 from pynput import keyboard
+import sys
+
+# thanks to : https://stackoverflow.com/questions/79419925/move-cursor-accross-multiple-lines-in-python
+
+listener = None
+
+
+def move_cursor_up():
+    sys.stdout.write("\033[1A")
+    sys.stdout.flush()
+
+
+def move_cursor_down():
+    sys.stdout.write("\033[1B")
+    sys.stdout.flush()
+
+
 lines = ""
 
-def write(line_count , no_of_lines):
+
+def write(line_count, no_of_lines):
     '''
     function:write
     parameters:line_count,no_of_lines
     returns:none
     '''
-    global lines
-    print(line_count,no_of_lines)
+    global lines, listener
+    if listener:
+        listener.stop()
+    print(line_count, no_of_lines)
     print(f"Default line count {no_of_lines}")
     print(f"Buffer Length {len(lines)}")
     print("press CTRL key to set line to 1")
     print("start typing")
     print("--------------------------------------")
-    # def on_ctrl_event(event):
-    #     nonlocal line_count
-    #     if event.name == 'ctrl':
-    #         line_count = 1
-    # keyboard.on_press(on_ctrl_event)
+
     def on_release(key):
         nonlocal line_count
         if key == keyboard.Key.ctrl:
             line_count = 1
-    # with keyboard.Listener(on_release=on_press) as listener:listener.join()
-    listener = keyboard.Listener(on_release=on_release)
+
+    def on_press(key):
+        if key == keyboard.Key.up:
+            move_cursor_up()
+        if key == keyboard.Key.down:
+            move_cursor_down()
+
+    listener = keyboard.Listener(on_release=on_release, on_press=on_press)
     listener.start()
     while line_count > 0:
         lines += input() + "\n"
@@ -44,33 +66,39 @@ def write(line_count , no_of_lines):
             if inc_line > 0:
                 no_of_lines += inc_line
                 line_count += inc_line
+
+
 def main():
     '''
     function:main
     returns:none
     main entry point
     '''
-    global lines
+    global lines, listener
     while True:
-        event = input("'n' to write ,'s' to save ,  'v' to view buffer , 'q' to exit: ")
-        if event == 's' and (len(lines)>1):
+        event = input(
+            "'n' to write ,'s' to save ,  'v' to view buffer , 'q' to exit: ")
+        if event == 's' and (len(lines) > 1):
             filename = input("filename>")
             path = input("path>")
             try:
-                with open(f"{path}{filename}", "w",encoding='utf-8') as file:
+                with open(f"{path}{filename}", "w", encoding='utf-8') as file:
                     file.write(lines)
                     print("Saved the content\nBuffer is cleared")
-            except  (OSError, FileExistsError)  as error:
+            except (OSError, FileExistsError) as error:
                 print(error)
         elif event == 'v' and len(lines) > 1:
             print(lines)
         elif event == 'q':
+            if listener:
+                listener.stop()
             break
         elif event == "n":
             lines = ""
-            write(5,5)
+            write(5, 5)
         else:
             print(f"Current Buffer length: {len(lines)} must be > than 1")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
