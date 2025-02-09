baf0db6605d0879cc4c9eae2a00bed2b84a67d28
samTime101 <srijanregmi44@gmail.com>
2025-02-09 22:53:34 +0545

    feat: open the file

--- main.py
+++ main.py
@@ -32,9 +32,10 @@ def write(line_count, no_of_lines):
     returns:none
     '''
     global lines, listener
+    # global listener
     if listener:
         listener.stop()
-    print(line_count, no_of_lines)
+    print(line_count, no_of_lines, lines)
     print(f"Default line count {no_of_lines}")
     print(f"Buffer Length {len(lines)}")
     print("press CTRL key to set line to 1")
@@ -68,6 +69,42 @@ def write(line_count, no_of_lines):
                 line_count += inc_line
 
 
+def open_in_buffer(line_count, no_of_lines, file_content):
+    global listener
+    if listener:
+        listener.stop()
+    global lines
+
+    def on_release(key):
+        nonlocal line_count
+        if key == keyboard.Key.ctrl:
+            line_count = 1
+
+    def on_press(key):
+        if key == keyboard.Key.up:
+            move_cursor_up()
+        if key == keyboard.Key.down:
+            move_cursor_down()
+
+    listener = keyboard.Listener(on_release=on_release, on_press=on_press)
+    listener.start()
+    for i in file_content:
+        lines += i
+    print(lines)
+    while line_count > 0:
+        lines += input() + "\n"
+        line_count -= 1
+        if line_count == 0:
+            print("last line")
+            try:
+                inc_line = int(input("<line> or 0: "))
+            except ValueError:
+                inc_line = 0
+            if inc_line > 0:
+                no_of_lines += inc_line
+                line_count += inc_line
+
+
 def main():
     '''
     function:main
@@ -77,7 +114,7 @@ def main():
     global lines, listener
     while True:
         event = input(
-            "'n' to write ,'s' to save ,  'v' to view buffer , 'q' to exit: ")
+            "'n' to write ,'o' to open file,'s' to save ,  'v' to view buffer , 'q' to exit: ")
         if event == 's' and (len(lines) > 1):
             filename = input("filename>")
             path = input("path>")
@@ -94,8 +131,19 @@ def main():
                 listener.stop()
             break
         elif event == "n":
-            lines = ""
+            line = ""
             write(5, 5)
+        elif event == "o":
+            filename = input("filename>")
+            path = input("path>")
+            try:
+                with open(f"{path}{filename}", "r", encoding='utf-8') as file:
+                    file_content = file.readlines()
+                    open_in_buffer(len(file_content), len(
+                        file_content), file_content)
+            except (OSError, FileExistsError) as error:
+                print(error)
+
         else:
             print(f"Current Buffer length: {len(lines)} must be > than 1")
 
